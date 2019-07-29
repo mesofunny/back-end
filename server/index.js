a259ca182f429d203bb006e05918971163bbf896
Rexben <rexben.rb@gmail.com>
2019-07-29 11:54:06 +0100

    add index route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.status(200).json('Welcome to Jokes API');
+});
+
 server.use('/api/v1', routes);
 
 module.exports = server;
